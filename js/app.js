2947b3ab4457bf1adb54b5336b605068a063821f
Nate Brochin <nbrochin@gmail.com>
2016-04-15 23:04:29 -0400

    new question sets

--- js/app.js
+++ js/app.js
@@ -1,45 +1,44 @@
-$(document).ready(function(){
+  $(document).ready(function(){
 
-  // var questions = [{
-  //   question: "Nikola Tesla's greatest contribution to science was which of the following achievements?",
-  //   choices: [' AC induction motor', ' The light bulb with tungsten filament', ' Underground electrical wires', ' The 3-pronged electrical outlet'],
-  //   correctAnswer: 0,
-  //   point_value: 5
-  // }, {
-  //   question: "Which of the following positions did Benjamin Franklin NOT hold?",
-  //   choices: [" Ambassador to France", " Newpaper editor in Philadelphia", " British postmaster for the colonies", " Inventor of the kite"],
-  //   correctAnswer: 3,
-  //   point_value: 15
-  // }, {
-  //   question: "What was the first message sent by Samuel Morse's telegraph machine?",
-  //   choices: [" Save our souls", " What hath God wrought?", " Mr. Watson, come here!", " Now is the time for all good men."],
-  //   correctAnswer: 1,
-  //   point_value: 10
-  // }, {
-  //   question: "This inventor of the Optical Analysis System was also the world's first Hispanic female astronaut. What is her name?",
-  //   choices: [ " Ellen Ochoa", " Sally Ride", " Linda Chavez", " Margaret Kingsley"],
-  //   correctAnswer: 0,
-  //   point_value: 25
-  // }, {
-  //   question: "Which Dutch inventor introduced the telescope to the world?",
-  //   choices: [" Hans Schopol", " Hans Lippershy", " Johann Bach", " Hans Holbein"],
-  //   correctAnswer: 1,
-  //   point_value: 20
+  var questions = [[
+  { question: "Nikola Tesla's greatest contribution to science was which of the following achievements?",
+    choices: [' AC induction motor', ' The light bulb with tungsten filament', ' Underground electrical wires', ' The 3-pronged electrical outlet'],
+    correctAnswer: 0,
+    point_value: 5 }, 
 
-  //   }, {
-  //   question: "The Niepce Crater on the Moon was named in recognition of the famous inventor Joseph Niepce. What did Niepce invent?",
-  //   choices: [ " Telescope", " Camera", " Microscope", " Lunar landing module"],
-  //   correctAnswer: 1,
-  //   point_value: 5
+  { question: "Which of the following positions did Benjamin Franklin NOT hold?",
+    choices: [" Ambassador to France", " Newpaper editor in Philadelphia", " British postmaster for the colonies", " Inventor of the kite"],
+    correctAnswer: 3,
+    point_value: 15
+  }, {
+    question: "What was the first message sent by Samuel Morse's telegraph machine?",
+    choices: [" Save our souls", " What hath God wrought?", " Mr. Watson, come here!", " Now is the time for all good men."],
+    correctAnswer: 1,
+    point_value: 10
+  }, {
+    question: "This inventor of the Optical Analysis System was also the world's first Hispanic female astronaut. What is her name?",
+    choices: [ " Ellen Ochoa", " Sally Ride", " Linda Chavez", " Margaret Kingsley"],
+    correctAnswer: 0,
+    point_value: 25
+  }, {
+    question: "Which Dutch inventor introduced the telescope to the world?",
+    choices: [" Hans Schopol", " Hans Lippershy", " Johann Bach", " Hans Holbein"],
+    correctAnswer: 1,
+    point_value: 20
 
-  //  }, {
-  // 	question: "The invention of the refrigerator made a significant improvement to our standard of living. Who is credited with the invention?",
-  //   choices: [" Carl von Linde", " Michael Faraday", " Oliver Evans", " Joseph Beuys"],
-  //   correctAnswer: 2,
-  //   point_value: 25
-  // }];
+     {
+    question: "The Niepce Crater on the Moon was named in recognition of the famous inventor Joseph Niepce. What did Niepce invent?",
+    choices: [ " Telescope", " Camera", " Microscope", " Lunar landing module"],
+    correctAnswer: 1,
+    point_value: 5
 
-  var questions_jetsons = [{
+   }, {
+  	question: "The invention of the refrigerator made a significant improvement to our standard of living. Who is credited with the invention?",
+    choices: [" Carl von Linde", " Michael Faraday", " Oliver Evans", " Joseph Beuys"],
+    correctAnswer: 2,
+    point_value: 25
+  }],
+  [ {
     question: "Meet George Jetson.... His boy ____",
     choices: [' George, Jr.', ' Cosmo', ' Elroy', ' Astro'],
     correctAnswer: 2,
@@ -76,16 +75,29 @@ $(document).ready(function(){
     choices: [" Cosmo's Rockets", " Spacely Sprockets", " Spacely's Rockets", " Astro-Glide"],
     correctAnswer: 1,
     point_value: 10
-  }];
-  
+  }]
+  ];
+ 
+  var questionSet = questions[0];
   var questionCounter = 0; //Tracks question number
   var selections = []; //Array containing user choices
-  var choices = questions_jetsons[0].correctAnswer;
+  var choices = questions[0,0].correctAnswer;
   var quiz = $('.trivia-questions'); //Quiz div object
   
+
+  $(".visible li").click(function() {
+  	questionSet = questions[$(this).data("set")];
+  	startGame();
+  	console.log(questionSet);
+  	console.log($(this).data("set"));
+
+  })
+
+
   // Display initial question
   displayNext();
   
+
   // Click handler for the 'next' button
   $('#next').on('click', function (e) {
     e.preventDefault();
@@ -109,9 +121,9 @@ $(document).ready(function(){
   	var numCorrect = 0;
     var totalPoints = 0;
     for (var i = 0; i < selections.length; i++) {
-      if (selections[i] === questions_jetsons[i].correctAnswer) {
+      if (selections[i] === questionSet[i].correctAnswer) {
         numCorrect++;
-        totalPoints+=questions_jetsons[i].point_value;
+        totalPoints+=questionSet[i].point_value;
     	
       }
 
@@ -129,31 +141,25 @@ $(document).ready(function(){
     });
 
     var currentScore = calculateScore();
-    var questionPtvalue = questions_jetsons[index].point_value;
+    var questionPtvalue = questionSet[index].point_value;
     $('#question-score.q-score-css').text((questionPtvalue) + 'pts');
 
-    if (questionPtvalue === 5) {
-    	$('ul#question-dots li:nth-child(' + (questionCounter+1) + ') i').css('color', '#009933');
-    } else if (questionPtvalue === 10) {
-    	$('ul#question-dots li:nth-child(' + (questionCounter+1) + ') i').css('color', '#003399');
-    } else if (questionPtvalue === 15) {
-    	$('ul#question-dots li:nth-child(' + (questionCounter+1) + ') i').css('color', '#ff9900');
+    if (selections[index] === questionSet[index].correctAnswer) {
+    	$('ul#question-dots li:nth-child(' + (questionCounter) + ') i').css('color', '#00ff33');
+    }     
+	
+	else  {
+    	$('ul#question-dots li:nth-child(' + (questionCounter) + ') i').css('color', '#ff3300');
  	}
-   	else if (questionPtvalue === 20) {
-    	$('ul#question-dots li:nth-child(' + (questionCounter+1) + ') i').css('color', '#cc3300');
-    }
-    else if (questionPtvalue == 25) {
-    	$('ul#question-dots li:nth-child(' + (questionCounter+1) + ') i').css('color', '#000000');
-    }
-    
+       
     var questionNo = $('<h2>QUESTION ' + 0 + (index + 1) + ':</h2>');
   	$('#question-number.question-01').html(questionNo);
     
-    var question = $('<div class="question-text">').append(questions_jetsons[index].question);
+    var question = $('<div class="question-text">').append(questionSet[index].question);
     qContainer.append(question);
     $('header ul li.total-score').text(currentScore[1]);
 
-
+	
     var radioButtons = createRadios(index);
     qContainer.append(radioButtons);
     
@@ -161,8 +167,6 @@ $(document).ready(function(){
   }
   
 
- 
-  
   // places user choice into an array
   function choose() {
     selections[questionCounter] = +$('input[name="answer"]:checked').val();
@@ -178,7 +182,10 @@ $(document).ready(function(){
       // $('header ul li.total-score').text("05");
 	  // $('.q-score-css').text("+10 pts");
       
-      if(questionCounter < questions_jetsons.length){
+
+
+
+      if(questionCounter < questionSet.length){
         var nextQuestion = createQuestioncontainer(questionCounter);
         quiz.append(nextQuestion).fadeIn();
         if (!(isNaN(selections[questionCounter]))) {
@@ -200,10 +207,10 @@ $(document).ready(function(){
     var radioList = $('<ul class="answer-text">');
     var item;
     var input = '';
-    for (var i = 0; i < questions_jetsons[index].choices.length; i++) {
+    for (var i = 0; i < questionSet[index].choices.length; i++) {
       item = $('<li>');
       input = '<input type="radio" name="answer" value=' + i + ' />';
-      input += questions_jetsons[index].choices[i];
+      input += questionSet[index].choices[i];
       item.append(input);
       radioList.append(item);
       // console.log(questions[index].choices[i]);
@@ -213,41 +220,44 @@ $(document).ready(function(){
     return radioList;
   }
 
-  var question01 = (questions_jetsons[0].question);
-  var question02 = (questions_jetsons[1].question);
-  var question03 = (questions_jetsons[2].question);
-  var question04 = (questions_jetsons[3].question);
-  var question05 = (questions_jetsons[4].question);
-  var question06 = (questions_jetsons[5].question);
-  var question07 = (questions_jetsons[6].question);
-  selections = [];
-
   // Calculates number of correct answers and creates a message to be displayed
   function displayScore(index) {
+  	// console.log(questionSet);
     var score = $('<div>',{class: 'trivia-questions'});
     var calculatedScore = calculateScore();
     currentScore = calculateScore();
     $('header ul li.total-score').text(currentScore[1]);
-    $('ul.answer-text').html(selections[0] + ' -- ' + questions_jetsons[0].correctAnswer + ' 01. ' + question01 + '<br />' + selections[1] + ' -- ' + questions_jetsons[1].correctAnswer + ' 02. ' + question02 + '<br />' + selections[2] + ' -- ' + questions_jetsons[2].correctAnswer + ' 03. ' + question03 + '<br />' + selections[3] + ' -- ' + questions_jetsons[3].correctAnswer + ' 04. ' + question04 + '<br />' + selections[4] + ' -- ' + questions_jetsons[4].correctAnswer + ' 05. ' + question05 + '<br />' + selections[5] + ' -- ' + questions_jetsons[5].correctAnswer + ' 06. ' + question06 + '<br />' + selections[5] + ' -- ' + questions_jetsons[6].correctAnswer + ' 06. ' + question07);
+    var result = "";
+    for (var i=0; i < questionSet.length; i++) {
+    	result += (i + 1 ) + '. ' + questionSet[i].question + '<br />' + 'Your Answer: ' + questionSet[i].choices[selections[i]]  +  ', <element style="color: black;">Correct Answer: </element>' + '<element style="color: #22aa22;">' + questionSet[i].choices[questionSet[i].correctAnswer] + '.</element><br />';
+
+    }
+  	$('ul.answer-text').html(result);
+    	// $('ul.answer-text').html('0' + i + '.' + question01 + '<br />' + 'Your Answer: ' + selections[i] + ' Correct Answer: ' + questionSet[i].correctAnswer + '<br />')
+    
+
 
    score.append('<h2>You\'ve answered ' + calculatedScore[0] + ' questions out of ' +
-   questions_jetsons.length + ' correctly. Your total score is ' + calculatedScore[1] + '.</h2></div>');
+   questionSet.length + ' correctly. Your total score is ' + calculatedScore[1] + '.</h2></div>');
     return score;
     }
- 
+   
   $('#start-game').click(function(){
-			event.preventDefault();
-			$('header ul li.total-score').text("0");
+  	startGame();
+ });
+
+  function startGame() {
+  	event.preventDefault();
+			// $('header ul li.total-score').text("18");
 			$('.q-score-css').text(" 0pts");
 			$('header ul li.topic-title').text('JETSONS TRIVIA');
 			$('header ul li.topic-title').css('animation-play-state','paused');
-			$('.fa-circle:nth-child(2)').css("color", "#778DA3" );
 			createQuestioncontainer(0);
 			$('.answer-text').hide();
 			questionCounter = 0;
 			selections = [];
 			displayNext();
- });
+  }
 
   /*--- Display information modal box ---*/
  		$(".app-instructions").click(function(){
@@ -257,6 +267,23 @@ $(document).ready(function(){
    		$(".overlay").show();
 
   	});
+/*--- Set and Maintain High Score List ---*/
+
+	var highScore = 1;
+	var myScore = calculateScore();
+	parseInt(localStorage.setItem('highScore', '22'));
+	var highScore = parseInt(localStorage.getItem('highScore'));
+	localStorage.getItem('highScore', 'myScore')
+
+			
+	console.log(highScore);
+	console.log(myScore);
+
+	return [myScore, highScore]
+
+
+
+		
 
   	/*--- Hide information modal box ---*/
   	$(".close").click(function(){
